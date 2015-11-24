5f06d0343adf6190b6d0cab8106333f3a8427ba3
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 17:43:45 +0000

    finished main outline of the program, need to work on making it nicer in the console

--- counter.rb
+++ counter.rb
@@ -3,6 +3,9 @@ require_relative './lib/palindrome_counter.rb'
 counter = PalindromeCounter.new
 
 Dir.glob('test_files/*.txt') do |test_file|
+  range_numbers = File.foreach(test_file).first(2)
+  palindrome_count = counter.count(range_numbers[0].to_i, range_numbers[1].to_i)
   puts "File checked: #{test_file}"
-  puts "\n"
+  puts "Range: #{range_numbers[0]}..#{range_numbers[1]}"
+  puts "Palindromes: #{palindrome_count}\n"
 end
