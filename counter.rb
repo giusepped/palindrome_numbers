fa9f9f028dec8bb2f48a233456bad1534a180cd1
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 18:09:05 +0000

    final commit for the console output, will still work on the readme

--- counter.rb
+++ counter.rb
@@ -4,11 +4,11 @@ counter = PalindromeCounter.new
 
 Dir.glob('test_files/*.txt') do |test_file|
   range_numbers = File.foreach(test_file).first(2)
-  start = range_numbers[0]
+  start = range_numbers[0].chomp
   finish = range_numbers[1]
   palindrome_count = counter.count(start.to_i, finish.to_i)
   puts "File checked: #{test_file}"
-  puts "Range: #{start}..#{finish}"
+  puts "Range: " + start + ".." + finish
   puts "Palindromes: #{palindrome_count}"
-  puts "\n"
+  puts
 end
