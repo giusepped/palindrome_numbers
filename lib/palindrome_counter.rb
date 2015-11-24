a67ffea60549f54a9e71eb9cc5aab8eba4212c89
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 17:25:14 +0000

    test green for count method for class PalindromeCounter

--- lib/palindrome_counter.rb
+++ lib/palindrome_counter.rb
@@ -1,6 +1,14 @@
 class PalindromeCounter
 
-  def initialize()
+  def initialize
     @counter = 0
   end
+
+  def count(start, finish)
+    (start..finish).each do |num|
+      @counter += 1 if num.to_s == num.to_s.reverse
+    end
+    @counter
+  end
+
 end
