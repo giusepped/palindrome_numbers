b334a0f786abec836af48c496de71ab4a629d7e8
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 17:37:12 +0000

    initialized PalindromeCounter object and started outline of the program

--- counter.rb
+++ counter.rb
@@ -1,1 +1,8 @@
 require_relative './lib/palindrome_counter.rb'
+
+counter = PalindromeCounter.new
+
+Dir.glob('test_files/*.txt') do |test_file|
+  puts "File checked: #{test_file}"
+  puts "\n"
+end
--- lib/palindrome_counter.rb
+++ lib/palindrome_counter.rb
@@ -1,10 +1,11 @@
 class PalindromeCounter
 
   def initialize
-    @counter = 0
+    @counter
   end
 
   def count(start, finish)
+    @counter = 0
     (start..finish).each do |num|
       @counter += 1 if num.to_s == num.to_s.reverse
     end
