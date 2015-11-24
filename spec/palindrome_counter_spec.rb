0a7642c83c926f583da2893f2bd5586564a7da6b
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 17:29:20 +0000

    added more tests for the palindrome counter class

--- spec/palindrome_counter_spec.rb
+++ spec/palindrome_counter_spec.rb
@@ -4,4 +4,12 @@ describe PalindromeCounter do
   it 'should return 18 when counting from 1 to 100' do
     expect(subject.count(1, 100)).to eq 18
   end
+
+  it 'should return 10 when counting from 1 to 20' do
+    expect(subject.count(1, 20)).to eq 10
+  end
+
+  it 'should return 90 when counting from 1000 to 10000' do
+    expect(subject.count(1000, 10000)).to eq 90
+  end
 end
